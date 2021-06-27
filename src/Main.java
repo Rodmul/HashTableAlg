8fab485369d94ce393d32f241b086d0ecb2acc08
Rodmul <alex.tyulin@gmail.com>
2021-06-27 13:05:59 +0300

    add hashxclass code

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    private static HashX[] hashArray;
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+    }
+}
+
+class HashX {
+    String key;
+    int value;
+
+    HashX(String key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
